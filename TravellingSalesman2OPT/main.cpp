d8bee5ad7e951cdad69c2cfa99d861e1eb3e78d9
xenocidewiki <xenocidebacon@gmail.com>
2018-11-21 17:25:33 +0100

    Fixes for final

--- TravellingSalesman2OPT/main.cpp
+++ TravellingSalesman2OPT/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <Windows.h>
+#include <time.h>
 
-int costMatrix[17][17] =
+
+const int number_of_cities = 1; //change this to how many cities you have
+int cities[number_of_cities] = { /*1,2,3,... */};
+
+int costMatrix[number_of_cities][number_of_cities] =
 {
-	//Add your cost adjacency matrix here and change its size
-
+/*
+Plug in your adjacency matrix here
+Format: 
+{ 0, 1, 0 },
+{ 1, 0, 0 },
+{ 0, 0, 0 }
+*/
 };
 
-int cities[17] = { //add your numbered cities here and change their size
-};
 
 int swap(int citi[], int i, int j)
 {
@@ -27,8 +35,9 @@ int main()
 	int size_of_array = sizeof(cities) / sizeof(*cities);
 	while (1) {
 		for (int i = 0; i < size_of_array - 1; i++) {
-			sum += costMatrix[cities[i] - 1][cities[i + 1] - 1];
+			sum += (costMatrix[cities[i] - 1][cities[i + 1] - 1]);
 		}
+		sum += costMatrix[cities[0]-1][cities[12]-1];
 
 		if (run_once == true) {
 			std::cout << sum << std::endl;
@@ -41,14 +50,14 @@ int main()
 			for (int x = 0; x < size_of_array; x++) {
 				std::cout << cities[x] << ", ";
 			}
-			std::cout << std::endl;
+			std::cout << cities[0] << std::endl;
 			bestsofar = sum;
 		}
 
 		sum = 0;
-
-		int rand1 = rand() % (0 - (size_of_array));
-		int rand2 = rand() % (0 - (size_of_array));
+		srand(time(NULL)); //comment this out if you want to sacrifice the randomness
+		int rand1 = rand() % ((size_of_array));
+		int rand2 = rand() % ((size_of_array));
 
 		if (rand1 == rand2)
 			continue;
